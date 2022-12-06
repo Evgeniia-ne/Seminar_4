ccbcd7642893350e6195c11d2e84b85b9d94c4d1
Asus <koncarik@mail.ru>
2022-12-06 22:13:32 +0500

    Добавлен коммит

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+int DataEntry(string str)
+
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int FindSum(int num)
+{
+    int sum = 0;
+
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int number = DataEntry("Введите число A: ");
+int result = FindSum(number);
+Console.WriteLine("Сумма всех чисел до А равна " + result);
---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Решение в группах задач:
+// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+Console.Clear();
+int DataEntry(string str)
+
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int Find(int num)
+{
+    int count = 0;
+
+    for (count = 0; num > 0; count++)
+    {
+        num = num/10;                  
+    }
+    return count;
+}
+
+int number = DataEntry("Введите число: ");
+int result = Find(number);
+Console.WriteLine("Количество цифр в числе: " + result);
