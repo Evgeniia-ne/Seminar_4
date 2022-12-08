9feb9e582f3944853bf0f8a267a1fdb57a6b58e0
Asus <koncarik@mail.ru>
2022-12-08 20:12:28 +0500

    3

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+Console.Clear();
+int DataEntry(string str)
+
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int FindSum(int num)
+{
+    int sum = 1;
+
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum * i;
+    }
+    return sum;
+}
+
+int number = DataEntry("Введите число N: ");
+int result = FindSum(number);
+Console.WriteLine($"{number} -> " + result);
